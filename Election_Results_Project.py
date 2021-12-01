5478c08610ba0be5c4d24ad162b6a116040418e9
Anamaria Huluba <82522372+anamaria-huluba@users.noreply.github.com>
2021-12-01 12:48:02 +0000

    Update Election_Results_Project.py

--- Election_Results_Project.py
+++ Election_Results_Project.py
@@ -1,8 +1,7 @@
 # Election Results
 
-# You’re part of an impartial research group that conducts phone surveys prior to local elections. 
-# During this election season, the group conducted a survey to determine how many people would vote 
-# for Cynthia Ceballos vs. Justin Kerrigan in the mayoral election.
+# You’re part of an impartial research group that conducts phone surveys prior to local elections. During this election season, the group conducted a survey to determine how many 
+# people would vote for Cynthia Ceballos vs. Justin Kerrigan in the mayoral election.
 
 # Now that the election has occurred, your group wants to compare the survey responses to the actual 
 # results.
@@ -14,8 +13,7 @@ import matplotlib as plt
 
 # 2. At the top of script.py is a list of the different survey responses.
 
-# Calculate the number of people who answered ‘Ceballos’ and save the answer to the variable 
-# total_ceballos.
+# Calculate the number of people who answered ‘Ceballos’ and save the answer to the variable total_ceballos.
 
 # Print the variable to the terminal to see its value.
 
@@ -28,8 +26,7 @@ total_ceballos = sum([1 for response in survey_responses if response == "Ceballo
 print(total_ceballos)
 # returns: 33
 
-# 3. Calculate the percentage of people in the survey who voted for Ceballos and save it to the variable 
-# percentage_ceballos.
+# 3. Calculate the percentage of people in the survey who voted for Ceballos and save it to the variable percentage_ceballos.
 
 # Print the variable to the terminal to see its value.
 
@@ -40,14 +37,11 @@ percentage_ceballos = total_ceballos / survey_length
 print(percentage_ceballos)
 # returns: 0.471428571429
 
-# 4. In the real election, 54% of the 10,000 town population voted for Cynthia Ceballos. Your supervisors
-# are concerned because this is a very different outcome than what the poll predicted. They want you to 
-# determine if there is something wrong with the poll or if given the sample size, it was an entirely 
-# reasonable result.
+# 4. In the real election, 54% of the 10,000 town population voted for Cynthia Ceballos. Your supervisors are concerned because this is a very different outcome than what 
+# the poll predicted. They want you to determine if there is something wrong with the poll or if given the sample size, it was an entirely reasonable result.
 
-# Generate a binomial distribution that takes the number of total survey responses, the actual success 
-# rate, and the size of the town’s population as its parameters. Then divide the distribution by the 
-# number of survey responses. Save your calculation to the variable possible_surveys.
+# Generate a binomial distribution that takes the number of total survey responses, the actual success rate, and the size of the town’s population as its parameters. Then 
+# divide the distribution by the number of survey responses. Save your calculation to the variable possible_surveys.
 
 possible_surveys = np.random.binomial(survey_length, 0.54, size=10000) / survey_length
 
@@ -55,11 +49,9 @@ possible_surveys = np.random.binomial(survey_length, 0.54, size=10000) / survey_
 
 plt.hist(possible_surveys, range=(0, 1), bins=20)
 plt.show()
-# 6. As we saw, 47% of people we surveyed said they would vote for Ceballos, but 54% of people voted 
-# for Ceballos in the actual election.
+# 6. As we saw, 47% of people we surveyed said they would vote for Ceballos, but 54% of people voted for Ceballos in the actual election.
 
-# Calculate the percentage of surveys that could have an outcome of Ceballos receiving less than 50% 
-# of the vote and save it to the variable ceballos_loss_surveys.
+# Calculate the percentage of surveys that could have an outcome of Ceballos receiving less than 50% of the vote and save it to the variable ceballos_loss_surveys.
 
 possible_surveys_length = float(len(possible_surveys))
 incorrect_predictions = len(possible_surveys[possible_surveys < 0.50])
@@ -67,14 +59,12 @@ ceballos_loss_surveys = incorrect_predictions / possible_surveys_length
 print(ceballos_loss_surveys)
 # returns: 0.2189, which means that there's about 20% chance that we get the wrong prediction. 
 
-# 7. With this current poll, about 20% of the time a survey output would predict Kerrigan winning, 
-# even if Ceballos won the actual election.
+# 7. With this current poll, about 20% of the time a survey output would predict Kerrigan winning, even if Ceballos won the actual election.
 
 # Your co-worker points out that your poll would be more accurate if it had more responders.
 
-# Generate another binomial distribution, but this time, see what would happen if you had instead 
-# surveyed 7,000 people. Divide the distribution by the size of the survey and save your findings to 
-# large_survey.
+# Generate another binomial distribution, but this time, see what would happen if you had instead surveyed 7,000 people. Divide the distribution by the size of the survey 
+# and save your findings to large_survey.
 
 large_survey_length = float(7000)
 large_survey = np.random.binomial(large_survey_length,0.54, size=10000) / large_survey_length 
@@ -83,8 +73,7 @@ plt.hist(possible_surveys, range=(0,1), bins=20)
 plt.hist(large_survey, alpha=0.5, range=(0,1), bins=20)
 plt.show()
 
-# 8. Now, recalculate the percentage of surveys that would have an outcome of Ceballos losing and save 
-# it to the variable ceballos_loss_new, and print the value to the terminal.
+# 8. Now, recalculate the percentage of surveys that would have an outcome of Ceballos losing and save it to the variable ceballos_loss_new, and print the value to the terminal.
 
 incorrect_predictions = len(large_survey[large_survey < 0.50])
 ceballos_loss_new = incorrect_predictions / large_survey_length
@@ -92,10 +81,9 @@ print(ceballos_loss_new)
 # returns: 0.0
 
 # What do we notice about this new value?
-#A much larger sample size is a much better representation of what the outcome of the resulta would be. 
+# A much larger sample size is a better representation of what the outcome of the results would be. 
 
 # What advice would you give to your supervisors about predicting results from surveys?
-# There are 0% chance of getting a result below 50% when using a more representative sample size, 
-# in our case of 7000. This clearly shows that a larger smaple size will result in more accurate 
-# predictions and less chance of failure. 
+# There are 0% chance of getting a result below 50% when using a more representative sample size, in our case of 7000. This clearly shows that a larger smaple size will 
+# result in more accurate predictions and less chances of failure. 
 
