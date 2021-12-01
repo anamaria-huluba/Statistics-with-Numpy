20e8c8d8edfc90839b74caa620498c903a05f3f6
Anamaria Huluba <82522372+anamaria-huluba@users.noreply.github.com>
2021-12-01 12:53:57 +0000

    Update Introduction_to_Statistics_with_Numpy.py

--- Introduction_to_Statistics_with_Numpy.py
+++ Introduction_to_Statistics_with_Numpy.py
@@ -1,25 +1,21 @@
 # Introduction to Numpy Review Project
 # 
-# 1. You are working on a team of climate scientists and one of your roles is to take the weekly temperature 
-# data and input into NumPy arrays for later analysis. The sensor records the temperature 4 times a day, 
-# at 0:00, 6:00, 12:00, and 18:00. 
-# 
+# 1. You are working on a team of climate scientists and one of your roles is to take the weekly temperature data and input into NumPy arrays for later analysis. 
+#The sensor records the temperature 4 times a day, at 0:00, 6:00, 12:00, and 18:00. 
+
 # You are given last weeks data (Monday through Friday) and asked to create a NumPy array.
 
 #We get started by importing the numpy package.
 
 import numpy as np
 
-# 2. Create an array temperatures by importing the data in the CSV temperature_data.csv
-
-# (CSV based on data from the National Oceanic and Atmospheric Administration)
+# 2. Create an array temperatures by importing the data in the CSV temperature_data.csv (CSV based on data from the National Oceanic and Atmospheric Administration)
 
 temperatures = np.genfromtxt('temperature_data.csv', delimiter=',')
 
 #3. Inspect the data by printing it to the terminal.
 
-# The columns are the times, starting at 0:00, and the rows are days, starting on Monday, and the values 
-# are the recorded temperatures at that time on those days.
+# The columns are the times, starting at 0:00, and the rows are days, starting on Monday, and the values are the recorded temperatures at that time on those days.
 
 print(temperatures)
 #returns: 
@@ -29,8 +25,7 @@ print(temperatures)
 #  [ 46.5  44.1  57.6  51.9]
 #  [ 46.2  43.9  57.2  51.5]]
 
-#4. One of the researchers noticed that the sensor had been incorrectly zeroed and all of the recorded 
-#temperatures are 3.0 degrees too cold.
+#4. One of the researchers noticed that the sensor had been incorrectly zeroed and all of the recorded temperatures are 3.0 degrees too cold.
 
 # Adjust the array so that the temperature readings are accurate and save them to temperatures_fixed.
 
@@ -44,8 +39,7 @@ print(temperatures_fixed)
 #  [ 49.5  47.1  60.6  54.9]
 #  [ 49.2  46.9  60.2  54.5]]
 
-# 5. Another researcher asked you for the temperature values from Monday. Save them to a new array, 
-#monday_temperatures.
+# 5. Another researcher asked you for the temperature values from Monday. Save them to a new array, monday_temperatures.
 
 monday_temperatures = temperatures_fixed[0, :]
 
@@ -53,8 +47,7 @@ print(monday_temperatures)
 #returns an array with the temperatures recorded on Monday:
 # [ 46.6  48.1  61.8  56. ]
 
-#6. “Hmmm, interesting” the researcher mumbles, “can you also give me the 6:00 AM data for Thursday and 
-# Friday?”
+#6. “Hmmm, interesting” the researcher mumbles, “can you also give me the 6:00 AM data for Thursday and Friday?”
 
 #Save this data to a new array thursday_friday_morning.
 
@@ -66,8 +59,7 @@ print(thursday_friday_morning)
 
 #7. Finally, the persistent researcher now wants all the high and low temperatures over the week.
 
-# Select all temperatures that are under 50 degrees or over 60 degrees and save them to a new array 
-# temperature_extremes.
+# Select all temperatures that are under 50 degrees or over 60 degrees and save them to a new array temperature_extremes.
 
 temperature_extremes = temperatures_fixed[(temperatures_fixed < 50) | (temperatures_fixed > 60)]
 
